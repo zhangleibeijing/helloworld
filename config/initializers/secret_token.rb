24df24f91d7621917e53bbb00c348570d8f3aa8c
lei zhang <lei.zhang@zuora.com>
2011-09-22 11:32:17 +0800

    add a new rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Repository::Application.config.secret_token = '8e99f5815794365888c81ab01cce45acb72b91ee97ab4ff7c6185b8a02706ced6628a4e40a04e092c9fba9445cdba6e37654a9be37d2604ff5ea2f88d0338214'
